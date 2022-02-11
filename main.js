eec2073987dff4790f4706a8e8d53b6b4cc07493
unknown <rotrast@gmail.com>
2022-02-11 23:15:10 +0200

    Added list of sizes

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+              ],
+            sizes: [
+                {id: 1, name: 'XS'},
+                {id: 2, name: 'S'},
+                {id: 3, name: 'M'},
+                {id: 4, name: 'L'},
+                {id: 5, name: 'XL'}
+            ]
         }
     }
 })
