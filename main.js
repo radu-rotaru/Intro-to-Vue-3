afa0989d7875cc7cdba928c0e90242b7b16c258e
unknown <rotrast@gmail.com>
2022-02-12 19:32:15 +0200

    Added decrement button

--- main.js
+++ main.js
@@ -11,5 +11,19 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        decrementCart() {
+            if(this.cart > 0)
+                this.cart -= 1
+            else
+                window.alert("The cart is empty")
+        }
     }
 })
